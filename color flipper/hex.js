9ee55c0baca83ad6b11d5caa4457eae7c6b95d31
Redoanul Haque <68851086+redoanul1995@users.noreply.github.com>
2022-08-10 08:59:25 +0600

    Update hex.js

--- color flipper/hex.js
+++ color flipper/hex.js
@@ -1,14 +1,1 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-
-
-document.querySelector("#btn").addEventListener("click", function(){
-    colorString = "#";
-    for(let i=0;i<6;i++){
-        let random = Math.floor(Math.random()*16);
-        colorString = colorString + hex[random];
-    }
-    document.body.style.background = colorString;
-    
-    document.querySelector(".color").textContent = colorString;
-})
